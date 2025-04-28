1c677b34275a380b263d7cb34ca9114602bfb885
Alex <olexandrvorona@gmail.com>
2025-04-28 12:22:52 +0200

    refactored table

--- lesson_2/src/App.jsx
+++ lesson_2/src/App.jsx
@@ -1,32 +1,9 @@
 import './App.css'
 import Table from './components/Table'
-import data from './assets/data'
-import { useState, useEffect } from 'react'
-
 
 function App() {
-  const [tableData, setTableData] = useState(structuredClone(data))
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-        const notActiveData = tableData.filter(data => !data.active)
-        if (notActiveData.length) {
-          const selectedData = notActiveData[Math.floor(Math.random()*notActiveData.length)]
-          selectedData.active = true;
-          const notActiveDataIndex = tableData.findIndex(data => data.type == selectedData.type &&
-            data.icon == selectedData.icon)
-          const newTableData = tableData.map((el, index) => index === notActiveDataIndex ? selectedData : el);
-          setTableData(newTableData)
-          console.log(`Updated another row.`)
-        } else {
-          clearInterval(intervalId);
-          console.log(`Stop updates.`)
-        }
-    }, 1000)
-  }, [])
-
   return (
-    <Table data={tableData} />
+    <Table />
   )
 }
 
--- lesson_2/src/components/Table.jsx
+++ lesson_2/src/components/Table.jsx
@@ -1,12 +1,34 @@
 import TableRow from "./TableRow"
+import data from '../assets/data'
+import { useState, useEffect } from 'react'
 
-const Table = ({data = []}) => {
+const Table = () => {
+    const [tableData, setTableData] = useState(structuredClone(data))
+
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+          const notActiveData = tableData.filter(data => !data.active)
+          if (notActiveData.length) {
+            const selectedData = notActiveData[Math.floor(Math.random()*notActiveData.length)]
+            selectedData.active = true;
+            const notActiveDataIndex = tableData.findIndex(data => data.type == selectedData.type &&
+              data.icon == selectedData.icon)
+            const newTableData = tableData.map((el, index) => index === notActiveDataIndex ? selectedData : el);
+            setTableData(newTableData)
+            console.log(`Updated another row.`)
+          } else {
+            clearInterval(intervalId);
+            console.log(`Stop updates.`)
+          }
+      }, 1000)
+    }, [])
+
     return (
         <table className="tbl">
             <tbody>
             {
-                Array.isArray(data) && data.length ? 
-                    data.map(row => <TableRow key={row.type} row={row}/>):
+                Array.isArray(tableData) && tableData.length ? 
+                tableData.map(row => <TableRow key={row.type} row={row}/>):
                     null
             }
             </tbody>
